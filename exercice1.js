fe31709645d71cecbbe8a4bd1ef81e0d41f6bb92
sophieclaisse <43751485+sophieclaisse@users.noreply.github.com>
2018-11-15 09:54:35 +0100

    Pense Bête tableaux JS - exercice simple

---
+++ exercice1.js
@@ -0,0 +1,42 @@
+
+var Tab= [];
+
+document.getElementById("OK").addEventListener("click", function (){
+
+    Tab.push(document.getElementById("input").value);
+    console.log(Tab);
+
+    if (Tab.length>=10){
+      document.getElementById("Valeur").innerHTML= Tab[Math.round(Math.random()*Tab.length)];
+      document.getElementById("Valeur5").innerHTML= Tab[4];
+
+    }
+
+    if (Tab.length<10){
+        document.getElementById("Valeur").innerHTML= "entrez au moins 10 valeurs"
+    }
+});
+
+
+document.getElementById("afficheRandom").addEventListener("click", function (){
+        document.getElementById("random").innerHTML= Tab[Math.round(Math.random()*Tab.length)];
+
+});
+
+document.getElementById("allTab").addEventListener("click", function (){
+    document.getElementById("all").innerHTML= Tab;
+
+});
+
+
+document.getElementById("supprEntree").addEventListener("click", function (){
+    Tab.pop();
+    document.getElementById("supE").innerHTML= Tab;
+
+});
+
+document.getElementById("supprTab").addEventListener("click", function (){
+    Tab= [];
+    console.log(Tab)
+    document.getElementById("supT").innerHTML= "Tableau vide" + Tab;
+});
